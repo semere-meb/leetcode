f9b19972491ad7e8f8343b89bf74ee929f1f9683
Semere M. Mebrahtom <semere.meharena1@gmail.com>
2026-02-21 20:54:55 +0100

    409._Longest_Palindrome.py

---
+++ 409._Longest_Palindrome.py
@@ -0,0 +1,9 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        sum = 0
+        rem = 0
+        for key in set(s):
+            sum += 2 * s.count(key)//2
+            if s.count(key) % 2 == 1:
+                rem = 1
+        return 2 * sum + rem
