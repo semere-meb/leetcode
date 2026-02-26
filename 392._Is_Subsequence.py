0692cf0bd8b31f72f7ef0d04d8108f00dc79fa07
Semere M. Mebrahtom <semere.meharena1@gmail.com>
2026-02-26 23:29:43 +0100

    Is Subsequence cleaned

--- 392._Is_Subsequence.py
+++ 392._Is_Subsequence.py
@@ -1,8 +1,6 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        index = -1
         for c in s:
-            print(f"testing '{c}' - '{t[index+1:]}'")
             if c not in t:
                 return False
             t = t[t.index(c)+1:]
